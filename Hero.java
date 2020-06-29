fc7c28969e9634b608315f84ede926660bcd766a
kenta-matsuzaki <kenta6910@gmail.com>
2020-06-29 14:49:56 +0900

    Hero.javaの更新

--- Hero.java
+++ Hero.java
@@ -12,6 +12,28 @@ public class Hero {
     this("ダミー");
   }
 
+  void bye(){
+    System.out.println("勇者は別れを告げた");
+  }
+
+  void die(){
+    System.out.println(this.name + "は死んでしまった！");
+  }
+
+  void attack(Matango m){
+    System.out.println(this.name + "の攻撃！");
+    System.out.println("お化けキノコ" + m.suffix + "から2ポイントの攻撃を受けた！");
+    this.hp -= 2;
+    if(this.hp <= 0){
+      this.die();
+    }
+  }
+
+  void sleep(){
+    this.hp = 100;
+    System.out.println(this.name + "は眠って回復した！");
+  }
+
   Sword sword;
   void attack(){
     System.out.println(this.name + "は攻撃した！");
